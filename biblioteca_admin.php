42a7b1343ee73c8f3c4dd004034cbf8ee80cac6c
diego Barrios <diegoarielbarriosmichalik@gmail.com>
2016-10-05 23:58:07 -0300

    bugs

--- biblioteca_admin.php
+++ biblioteca_admin.php
@@ -16,45 +16,40 @@ if ($_SESSION['last_access'] == "1") {
     $ok = 1;
     $menu = $_SESSION['menu'];
 }
-
-
 $q = null; //Initialization value; Examples
 $q = isset($_GET['q']) ? $_GET['q'] : '';
 $q = !empty($_GET['q']) ? $_GET['q'] : '';
-
-
 if ($ok == 1) {
     ?>
     <!DOCTYPE HTML>
     <html>
         <head>
             <title>Biblioteca</title>
-
-
             <link href = "css2/bootstrap.css" rel = "stylesheet" type = "text/css" media = "all">
-            <!--jQuery (necessary for Bootstrap's JavaScript plugins) -->
             <script src="js/jquery-1.11.0.min.js"></script>
-            <!-- Custom Theme files -->
             <link href="css2/style.css" rel="stylesheet" type="text/css" media="all"/>
-            <!-- Custom Theme files -->
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-            <meta name="keywords" content="Training Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
-                  Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
-            <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-            <!--Google Fonts-->
+            <meta name="keywords" content="" />
+            <script type="application/x-javascript"> 
+                addEventListener("load", 
+                function() { setTimeout(hideURLbar, 0); }, 
+                false); 
+                function hideURLbar(){ 
+                window.scrollTo(0,1); 
+                } 
+            </script>
             <link href='//fonts.googleapis.com/css?family=Asap:400,700' rel='stylesheet' type='text/css'>
             <link href = '//fonts.googleapis.com/css?family=Kreon:400,700,300' rel = 'stylesheet' type = 'text/css'>
-            <!--start-smoth-scrolling -->
             <script type = "text/javascript" src = "js/move-top.js"></script>
             <script type="text/javascript" src="js/easing.js"></script>
             <script type="text/javascript">
                 jQuery(document).ready(function ($) {
-                    $(".scroll").click(function (event) {
-                        event.preventDefault();
-                        $('html,body').animate({scrollTop: $(this.hash).offset().top}, 1000);
-                    });
+                $(".scroll").click(function (event) {
+                event.preventDefault();
+                $('html,body').animate({scrollTop: $(this.hash).offset().top}, 1000);
                 });
+            });
             </script>
             <!-- //end-smoth-scrolling -->
             <!--light-box-files -->
@@ -62,9 +57,9 @@ if ($ok == 1) {
             <link rel="stylesheet" href="css/chocolat.css" type="text/css" media="screen" charset="utf-8">
             <!--light-box-files -->
             <script type="text/javascript" charset="utf-8">
-                $(function () {
-                    $('.gallery-grid a').Chocolat();
-                });
+    $(function () {
+        $('.gallery-grid a').Chocolat();
+    });
             </script>
 
             <script src="js/bootstrap.min.js"></script>
@@ -76,7 +71,7 @@ if ($ok == 1) {
             <div class="banner2">
                 <div class="container">
                     <div class="header">
-                        <?php include './header.html'; ?>        
+    <?php include './header.html'; ?>        
                     </div>
                     <div class="top-nav">
                         <div class="navbar-header">
@@ -108,54 +103,35 @@ if ($ok == 1) {
                     <fieldset></fieldset>
                 </div>
             </div>
-
             <?php
             if ($q == 'add_book') {
                 ?>
                 <div class="gallery" id="gallery">
-
                     <div class="container">
-
                         <div class="gallery-main">
-
                             <div class="gallery-bott">
-
                                 <form enctype="multipart/form-data" action="libro_save.php" method="POST">
-                                    <!--<form action="" method="post" name="formulario">-->
-
-
                                     <strong> I.S.B.N.&nbsp; </strong> <input name="isbn" type="text" size="80" required="" placeholder="Ingrese ISBN"/> 
-                                    <br>
-                                    <br>
+                                    <br><br>
                                     <strong> Título &nbsp; </strong>
                                     <input type="text"   name="titulo"  size="80" required="" placeholder="Ingrese un titulo">
-                                    <br>
-                                    <br>
+                                    <br><br>
                                     <strong> Descripción </strong>
                                     <input type="text" name="subtitulo" size="76" required="" placeholder="" >
-                                    <br>
-                                    <br>
-
-
+                                    <br><br>
                                     <strong> Autor </strong>
                                     <select name="id_autor" required="">
                                         <option value="" >Elija un autor</option>
                                         <option value="1" >Autor 1</option>
                                     </select>
-                                    <br>
-                                    <br>
+                                    <br><br>
                                     <strong> Nro. de Páginas </strong>
                                     <input type="number"   name="paginas" required=""  >
-                                    <br>
-                                    <br>
-
+                                    <br><br>
                                     <input type="file" name="image"  required="" />
-
-                                    <br>
-                                    <br>
+                                    <br><br>
                                     <input type="submit" value="Guardar" />
-                                    <br>
-                                    <br>
+                                    <br><br>
                                 </form>
                             </div>
                         </div>
@@ -164,37 +140,29 @@ if ($ok == 1) {
                 <?php
             } else {
                 ?>
-
-
                 <div class="gallery" id="gallery">
-
                     <div class="container">
-
                         <div class="gallery-main">
-
                             <div class="gallery-bott">
-
                                 <?php
-                                $contenido = null; //Initialization value; Examples
+                                $contenido = null;
                                 $contenido = isset($_POST['buscar']) ? $_POST['buscar'] : '';
                                 $contenido = !empty($_POST['buscar']) ? $_POST['buscar'] : '';
                                 if ($contenido != null) {
-
                                     $dbh = new PDO('pgsql:host=localhost;port=5432;dbname=biblioteca;user=postgres;password=postgres');
-                                    foreach ($dbh->query("SELECT * from libro inner join editorial on editorial.id_editorial = libro.id_editorial where titulo ilike '%$contenido%' ") as $fila) {
+                                    foreach ($dbh->query(""
+                                            . "SELECT * from libro "
+                                            . "inner join editorial on editorial.id_editorial = libro.id_editorial "
+                                            . "where titulo ilike '%$contenido%' ") as $fila) {
                                         $id = $fila['id_libro'];
-                                        $exif = exif_read_data("images/".$id.".jpg", 'IFD0');
-                                        $exif = exif_read_data("images/".$id.".jpg", 0, true);
+                                        $exif = exif_read_data("images/" . $id . ".jpg", 'IFD0');
+                                        $exif = exif_read_data("images/" . $id . ".jpg", 0, true);
                                         foreach ($exif as $clave => $sección) {
                                             foreach ($sección as $nombre => $var) {
                                                 if (is_string($var)) {
                                                     $nuevo = f_remove_odd_characters($var);
-//                                                    if ($nuevo == $contenido) {
-//                                                        echo "YES <br>";
-//                                                    }
                                                     $resultado = strpos($nuevo, $contenido);
                                                     if ($resultado !== FALSE) {
-//                                                        echo $nuevo. $nombre . "<br>";
                                                         ?>
                                                         <div class="col-md-4 col1 gallery-grid">
                                                             <figure class="effect-bubba">
@@ -209,7 +177,6 @@ if ($ok == 1) {
                                                         </div>
                                                         <?php
                                                     }
-
                                                     $nuevo = "";
                                                 }
                                             }
@@ -222,11 +189,9 @@ if ($ok == 1) {
                         </div>
                     </div>
                 </div>
-
-            <?php } ?>
+    <?php } ?>
         </body>
     </html>
-
     <?php
 } else {
     header('Location: login.php');
